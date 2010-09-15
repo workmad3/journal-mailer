11cf0cf05d82f2c04a8452c729a19dbe2cfe3d7b
David Workman <workmad3@gmail.com>
2010-09-15 19:24:15 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JournalMailer::Application.config.secret_token = '8c6949075fd8666555bd7c700c1c5c95ee5e8c7e618416af82c5addcd341a8a161059931456675c9b225d689b937e28c8917a07ff7f11505afee1f99d7275c57'
